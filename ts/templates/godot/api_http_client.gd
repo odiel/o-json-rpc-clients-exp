7b822bae9094146db00788c7c68e8b28442c7d3f
Odiel Leon <odiel.leon@gmail.com>
2026-07-12 07:59:17 -0400

    - updated the Godot client template

--- ts/templates/godot/api_http_client.gd
+++ ts/templates/godot/api_http_client.gd
@@ -8,11 +8,6 @@ var server_url: String
 var option_request_timeout: int = 1
 var option_log_level: int = 1 # 0 Debug; 1 Info; 2 Warning; 3 Error
 
-signal on_request_failure(error: HTTPRequest.Result)
-signal on_server_error(code: int)
-signal on_invalid_response(response: String)
-signal on_response(response: Variant)
-
 var _registered_procedures: Array[ProcedureRequest] = []
 
 func _init(host: String, port: int, options: Dictionary[String, Variant] = {}):
@@ -27,7 +22,6 @@ func _init(host: String, port: int, options: Dictionary[String, Variant] = {}):
 func send(options: Dictionary[String, Variant] = {}) -> ORPC_HTTP_Client_${apiSlug}:
     var http_request = HTTPRequest.new()
     add_child(http_request)
-    http_request.request_completed.connect(_on_request_done)
     http_request.timeout = option_request_timeout
 
     var headers = ["Content-Type: application/json"]
@@ -38,13 +32,42 @@ func send(options: Dictionary[String, Variant] = {}) -> ORPC_HTTP_Client_${apiSl
         print("[DEBUG] Sending payload to %s" % server_url)
         print(json_string)
 
-    var error = http_request.request(server_url, headers, HTTPClient.METHOD_POST, json_string)
+	var error = http_request.request(server_url, headers, HTTPClient.METHOD_POST, json_string)
     if error != OK:
+		if option_log_level <= 3:
+			print("[ERROR] Request failed with error code %d" % error)
+
+		return Response.create_error("request_failure", error)
+
+    var network_result = await http_request.request_completed
+	var result: int = network_result[0]
+    var response_code: int = network_result[1]
+    var body: PackedByteArray = network_result[3]
+
+    http_request.queue_free()
+	_registered_procedures.clear()
+
+    if result != HTTPRequest.RESULT_SUCCESS:
+	    return Response.create_error("request_failure", "%s" % result)
+
+    if response_code < 200 or response_code >= 300:
+		return Response.create_error("server_failure", "%s" % response_code)
+
+	var response_string = body.get_string_from_utf8()
+    var json = JSON.new()
+    var response_payload = json.parse(response_string)
+
+    if response_payload != OK:
         if option_log_level <= 3:
-            print("[ERROR] Request failed with error code %d" % error)
-        on_request_failure.emit(error)
-        queue_free()
-    return self
+            print("[ERROR] Invalid server response content")
+            print(response_string)
+        return Response.create_error("invalid_response", response_string)
+
+    if option_log_level < 2:
+        print("[DEBUG] Response payload")
+        print(response_string)
+
+    return Response.create_success(json.get_data())
 
 func add_procedure(name: String, id: String = "", input: Variant = null) -> ORPC_HTTP_Client_${apiSlug}:
     if id == "":
@@ -56,36 +79,6 @@ func add_procedure(name: String, id: String = "", input: Variant = null) -> ORPC
 
 # replace: proceduresCode
 
-func _on_request_done(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
-    _registered_procedures = []
-
-    if result != HTTPRequest.RESULT_SUCCESS:
-        if option_log_level <= 3:
-            print("[ERROR] Request failed with error code %d" % result)
-        on_request_failure.emit(result)
-    elif response_code < 200 or response_code >= 300:
-        if option_log_level <= 3:
-            print("[ERROR] Server response code %d" % response_code)
-        on_server_error.emit(response_code)
-    else:
-        var response_string = body.get_string_from_utf8()
-        var json = JSON.new()
-        var parse_err = json.parse(response_string)
-
-        if parse_err == OK:
-            var payload = json.get_data()
-            on_response.emit(payload)
-            if option_log_level < 2:
-                print("[DEBUG] Response payload")
-                print(payload)
-        else:
-            if option_log_level <= 3:
-                print("[ERROR] Invalid server response content")
-                print(response_string)
-            on_invalid_response.emit(response_string)
-
-    queue_free()
-
 func _build_request_payload(options: Dictionary[String, Variant] = {}) -> Variant :
     var procedures_json: Array[Variant] = []
     for procedure in _registered_procedures:
@@ -93,10 +86,8 @@ func _build_request_payload(options: Dictionary[String, Variant] = {}) -> Varian
 
     var options_payload = {}
 
-    if options.has("authentication"):
-        options_payload["authentication"] = options.authentication
-    if options.has("execution"):
-        options_payload["execution"] = options.execution
+	if options:
+		options_payload = options.to_payload()
 
     var payload = {
         "protocol": "v1",
@@ -120,12 +111,106 @@ class ProcedureRequest:
         input = p_input
 
     func to_simple_obj() -> Variant:
-        var json = {
-            "id": id,
-            "name": name,
-        }
+        var json = {"id": id, "name": name }
 
         if input:
             json["input"] = input
 
         return json
+
+class Response:
+    var is_ok: bool = false
+    var error_type: String = ""
+    var error_code: String = ""
+    var data: Variant = null
+
+    static func create_success(p_data: Variant) -> Response:
+        var res = Response.new()
+        res.is_ok = true
+        res.data = p_data
+        return res
+
+    static func create_error(type: String, code: String) -> Response:
+        var res = Response.new()
+        res.is_ok = false
+        res.error_type = type
+        res.error_code = code
+        return res
+
+class RequestOptions:
+    var authentication: RequestAuthentication
+
+    static func with_authentication(authentication: RequestAuthentication) -> RequestOptions:
+        var res = RequestOptions.new()
+        res.authentication = authentication
+        return res
+
+    func to_payload() -> Variant:
+        var payload = {}
+
+        if (authentication):
+            payload["authentication"] = authentication.to_payload()
+
+        return payload
+
+class RequestExecution:
+    var strategy: String = "parallel"
+    var procedure_timeout: int = 5
+
+    static func with_sequential_strategy(procedure_timeout: int = 5) -> RequestExecution:
+        var res = RequestExecution.new()
+        res.strategy = "sequential"
+        res.procedure_timeout = procedure_timeout
+        return res
+
+    static func with_parallel_strategy(procedure_timeout: int = 5) -> RequestExecution:
+        var res = RequestExecution.new()
+        res.strategy = "parallel"
+        res.procedure_timeout = procedure_timeout
+        return res
+
+class RequestAuthentication:
+    var scheme: String = ""
+    var token: String = ""
+    var token_type: String = ""
+    var provider: String = ""
+
+    static func with_session(token: String, token_type: String) -> RequestAuthentication:
+        var res = RequestAuthentication.new()
+        res.scheme = "session"
+        res.token = token
+        res.token_type = token_type
+        return res
+
+    static func with_access_token(token: String) -> RequestAuthentication:
+        var res = RequestAuthentication.new()
+        res.scheme = "access_token"
+        res.token = token
+        res.token_type = "jwt"
+        return res
+
+    static func with_refresh_token(token: String) -> RequestAuthentication:
+        var res = RequestAuthentication.new()
+        res.scheme = "refresh_token"
+        res.token = token
+        res.token_type = "jwt"
+        return res
+
+    static func with_identity_provider(token: String, token_type: String, provider: String) -> RequestAuthentication:
+        var res = RequestAuthentication.new()
+        res.scheme = "identity_provider"
+        res.token = token
+        res.token_type = token_type
+        return res
+
+    func to_payload() -> Variant:
+        var payload = {
+            "scheme": scheme,
+            "token": token,
+            "token_type": token_type
+        }
+
+        if provider:
+            payload["provider"] = provider
+
+        return payload
